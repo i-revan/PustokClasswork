38cd355328208221cd5abcf8de27fc9de4c63390
i-revan <rvaniskndrov03@gmail.com>
2023-05-10 13:38:34 +0400

    added Pustok Classwork

---
+++ Areas/PustokAdmin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PustokClassWork.Areas.PustokAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+namespace PustokClassWork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{area:exists}/{controller=home}/{action=index}/{id?}"
+                );
+            app.MapControllerRoute(
+                name:"default",
+                pattern: "{controller=home}/{action=index}/{id?}"
+                );
+
+            app.Run();
+        }
+    }
+}
